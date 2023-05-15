9ccd8b5a84642ba9136d5f2897e04dd4d3d44b86
Enmanuel Rodriguez <enmanuelrodmac@gmail.com>
2023-05-15 19:25:59 -0400

    Websocket server basico

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -6,7 +6,7 @@ const btnEnviar = document.querySelector('#btnEnviar')
 const socket = io();
 
 socket.on('connect', ()=>{
-    console.log('Conectado al servidor');
+    // console.log('Conectado al servidor');
     lblOnline.style.display = 'block';
     lblOffline.style.display = 'none';
 })
@@ -18,10 +18,22 @@ socket.on('disconnect', ()=>{
     
 })
 
+socket.on('enviar-mensaje', (payload)=>{
+    // console.log('holaa');
+    console.log(payload);
+})
+
 btnEnviar.addEventListener('click', ()=>{
 
     const mensaje = txtMensaje.value;
+    const payload = {
+        mensaje,
+        id: '123ABC',
+        fecha: new Date().getTime()
+    }
 
-    socket.emit( 'enviar-mensaje', mensaje )
+    socket.emit( 'enviar-mensaje', payload, (id)=>{
+        console.log('Desde el server', id);
+    } )
 
 })
